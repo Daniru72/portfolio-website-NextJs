452c0d7f4bc983d1fc04f4660e696088cdf89763
Daniru72 <Daniru@dev.athenaholdingsllc.com>
2025-06-12 11:52:36 +0530

    Service design

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,43 @@
+//icon
+// import { } from 'react-icon/rx';
+
+//server data
+
+
+//components
+import ServiceSlider from '../../components/ServiceSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+
+//framer motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+
+  <div className='h-full bg-primary/30 py-36 flex items-center'>
+
+  <Circles/>
+  <div className='container mx-auto'>
+
+    <div className='flex flex-col xl:flex-row gap-x-8'>
+      {/* text */}
+      <div>text</div>
+      {/* slider */}
+      <ServiceSlider/>
+    </div>
+  </div>
+
+  <Bulb/>
+
+  </div>
+
+
+
+  );
+  
+ 
 };
 
 export default Services;
