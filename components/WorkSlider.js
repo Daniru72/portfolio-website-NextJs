2bf560ab5bbed2f4cf8f0fef6ccd62910b468441
Daniru72 <Daniru@dev.athenaholdingsllc.com>
2025-06-12 21:03:19 +0530

    My Work design completed

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -62,6 +62,7 @@ import {BsArrowRight} from "react-icons/bs";
 
 //import required modules
 import { Pagination } from 'swiper';
+import Image from 'next/image';
 
 
 
@@ -81,7 +82,32 @@ const WorkSlider = () => {
       {workSlides.slides.map((slide, index) => {
         return(
             <SwiperSlide key={index} >
-               slide
+               <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+                {slide.images.map((image, index) => {
+                  return(
+                    <div className='relative rounded-lg overflow-hidden flex items-center justify-center group' key={index}>
+                       <div className='flex items-center justify-center relative overflow-hidden group'>
+                        {/* image */}
+                          <Image src={image.path} width={500} height={300} alt='' />
+                          {/* overlay gradiant */}
+                          <div className='absolute inset-0 bg-gradient-to-l from-transparent via-black/60 to-black opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+                          {/* title */}
+                          <div className='absolute bottom-0 translate-y-full group-hover:xl:-translate-y-20 transition-all duration-300'>
+                            <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+                              {/* title part 1 */}
+                              <div className='delay-100'>LIVE</div>
+                              {/* title part 2 */}
+                              <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
+
+                              <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'><BsArrowRight/></div>
+                            </div>
+                          </div>
+                       </div>
+                    </div>
+                   
+                  );
+                })}
+               </div>
             </SwiperSlide>
         );
       })}
